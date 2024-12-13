75bb487bbd6fb95c7029cafad3678e6b01f2e09b
kidneyweakx <kidneyweakx@gmail.com>
2024-12-14 01:47:34 +0800

    fix(pwa): polyfill for solana web3js

--- apps/pwa/vite.config.ts
+++ apps/pwa/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig, AliasOptions } from "vite";
 import path from "path";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 const root = path.resolve(__dirname, "src");
 
 // https://vitejs.dev/config/
@@ -23,6 +24,10 @@ export default defineConfig({
       domains: ["*.localhost"],
       /** custom certification directory */
       certDir: "/Users/kidneyweakx/.devServer/cert",
+    }),
+    nodePolyfills({
+      // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
+      include: ["crypto"],
     }),
   ],
   resolve: {
