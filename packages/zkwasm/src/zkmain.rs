fa79803db03bd3b65d9d0509be8f802928b959ee
kidneyweakx <kidneyweakx@gmail.com>
2024-12-14 14:18:16 +0800

    feat(zkeasm): final version to check merits plan

--- packages/zkwasm/src/zkmain.rs
+++ packages/zkwasm/src/zkmain.rs
@@ -5,7 +5,7 @@ use zkwasm_rust_sdk::wasm_output;
 
 use crate::definition::RustGameState;
 
-use crate::gameplay::{_get_game_state, initialize_game, step};
+use crate::gameplay::{_get_game_state, initialize_game, add_merits};
 
 pub fn spin() {
     // specify the public inputs
@@ -19,13 +19,13 @@ pub fn spin() {
 
     for _i in 0..private_inputs_length {
         let _private_input = unsafe { wasm_input(0) };
-        step(_private_input);
+        add_merits(_private_input);
     }
 }
 
 /*
     PUBLIC INPUTS marked by `wasm_input`: e.g wasm_input(1) specifies a public input of type u64
-    PRIVATE INPUTS marked by `wasm_input`: e.g wasm_input(0) specifies a priva  te input of type u64
+    PRIVATE INPUTS marked by `wasm_input`: e.g wasm_input(0) specifies a private input of type u64
     PUBLIC OUTPUTS marked by `wasm_output`: e.g wasm_output(var) specifies an output `var` of type u64
 */
 #[wasm_bindgen]
@@ -39,6 +39,7 @@ pub fn zkmain() -> i64 {
         // specify the output
         wasm_output(final_game_state.total_steps as u64);
         wasm_output(final_game_state.current_position as u64);
+        wasm_output(final_game_state.merits as u64); // 輸出 merits
     }
 
     0
